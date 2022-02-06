fb83878d7f89dfc905eb9f309549f7ad515b043a
Stefan Atanasov <stefan.atanasov@ffwagency.com>
2022-02-06 10:16:43 +0200

    Fix regex matching, start parsing params properly

--- rename_files.py
+++ rename_files.py
@@ -1,26 +1,38 @@
 #!/usr/bin/python
 
+from genericpath import isdir
 import sys
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, sep
 import re
+import argparse
 
+def browse_dir(path, regEx, files = []):
+    for file in listdir(path):
+      file_candidate = join(path, file);
+      if isdir(file_candidate) and file != ".git": #lame
+        browse_dir(file_candidate, regEx, files)
+        continue
+      if isfile(file_candidate) and bool(re.search(regEx, file)):
+        files.append(file_candidate)
+    
 def rename_files(path, regEx):
     files = []
-    for file in listdir(path):
-      if isfile(file) and bool(re.match(regEx, file)):
-        files.append(file)
+    browse_dir(path, regEx, files)
     print(files)
-    # onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
-    
-
-
-print ('Number of arguments:', len(sys.argv), 'arguments.')
-print ('Argument List:', str(sys.argv))
 
 try:
-    path = sys.argv[1]
-    regEx = sys.argv[2]
+    # Construct the argument parser
+    ap = argparse.ArgumentParser()
+
+    # Add the arguments to the parser
+    ap.add_argument("-d", "--dir", required=True,
+    help="Default is current folder")
+    ap.add_argument("-r", "--regex", required=True,
+    help="Default is *")
+    args = vars(ap.parse_args())
+    path = args['dir']
+    regEx = args['regex']
 except IndexError:
     print('Path or regEx is not provided')
 else:
